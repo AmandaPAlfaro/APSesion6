ab8654ed686ba457a47564018339963024fd1cd0
AmandaPAlfaro <amanda.perez.captiva@outlook.com>
2025-04-08 17:37:49 -0400

    Patron Observer

---
+++ Sesion6/Sesion6/Program.cs
@@ -0,0 +1,21 @@
+﻿using Sesion6.Patrones;
+
+Console.WriteLine("Patrones de Comportamiento");
+
+Console.WriteLine("Seleccione el patron a ejecutar");
+
+Console.WriteLine("1. Observer");
+
+var opcion = Console.ReadLine();
+
+switch (opcion) 
+{
+    case "1":
+        Console.WriteLine("Patron Observador");
+        AppObserver.Ejecutar();
+        break;
+
+    case "2":
+        Console.WriteLine("Opcion no valida");
+        break;
+}
